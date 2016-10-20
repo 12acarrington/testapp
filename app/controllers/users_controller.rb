12c560b8d20201b3bcc0365e26dfe9f522d8cb41
Ashley Carrington <ashley.r.carrington@gmail.com>
2016-10-20 12:55:41 +0100

    edited the user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,29 +25,23 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'Your account has been created! Welcome to Twooter!' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+   
+    if @user.save
+      sign_in @user
+      redirect_to @user, notice: 'Welcome to Twooter!'
+    else
+      render :new
     end
   end
+   
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit 
     end
   end
 
@@ -55,10 +49,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
